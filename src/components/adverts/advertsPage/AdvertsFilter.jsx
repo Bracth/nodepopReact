5c1762efb7c65b4050ea8ec8a47b1d89bf354a93
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-03-23 19:38:00 +0100

    Adding radio layout to the filterComponent

--- src/components/adverts/advertsPage/AdvertsFilter.jsx
+++ src/components/adverts/advertsPage/AdvertsFilter.jsx
@@ -24,6 +24,30 @@ function AdvertsFilter({ handleFilter }) {
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Control value={name} onChange={handleChange} name="name"  type="text" placeholder="Enter advert name" />
         </Form.Group>
+        
+    <div key={`inline-radio`} className="mb-3">
+      <Form.Check
+        inline
+        label="ALL"
+        name="group1"
+        type="radio"
+        id={`inline-radio-1`}
+      />
+      <Form.Check
+        inline
+        label="SALE"
+        name="group1"
+        type="radio"
+        id={`inline-radio-2`}
+      />
+      <Form.Check
+        inline
+        label="BUY"
+        name="group1"
+        type="radio"
+        id={`inline-radio-3`}
+            />
+            </div>
         </Form>
 }
 
