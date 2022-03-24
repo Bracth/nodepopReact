10618fb9a86bba85e28164b864a2cceb7f7d66e9
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-03-24 19:41:05 +0100

    Creating tags inputs

--- src/components/adverts/advertsPage/AdvertsFilter.jsx
+++ src/components/adverts/advertsPage/AdvertsFilter.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form"
+import Stack from "react-bootstrap/Stack"
 
 
 function AdvertsFilter({ handleFilter }) {
@@ -8,15 +9,22 @@ function AdvertsFilter({ handleFilter }) {
         name: "",
         sale: "all",
         minPrice: 1,
-        maxPrice: 10000
+        maxPrice: 10000,
+        lifestyle: false,
+        motor: false,
+        mobile: false,
+        work: false
     });
     
-    const { name, sale, minPrice, maxPrice } = filters;
+    const { name, sale, minPrice, maxPrice, lifestyle, motor, mobile, work } = filters;
     
     const handleChange = (event) => {
         setFilters(filters => ({
             ...filters,
-            [event.target.name]: event.target.value
+            [event.target.name]:
+                event.target.type === "checkbox" ?
+                    event.target.checked
+                   : event.target.value
         }))
     }
     
@@ -66,7 +74,27 @@ function AdvertsFilter({ handleFilter }) {
         <Form.Range onChange={handleChange} value={minPrice} min={1} max={maxPrice } name="minPrice"/>
         
         <Form.Label>Max Price: { maxPrice }€</Form.Label>
-        <Form.Range onChange={handleChange} value={maxPrice} min={minPrice} max={10000 } name="maxPrice"/>    
+        <Form.Range onChange={handleChange} value={maxPrice} min={minPrice} max={10000} name="maxPrice" />    
+            
+            <Stack direction="horizontal" gap={3}>
+                <Form.Group className="mb-3" controlId="lifestyle">
+                    <Form.Check type="checkbox" label="lifestyle" name="lifestyle" value="lifestyle" checked={lifestyle} onChange={handleChange} inline/>
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="mobile">
+                <Form.Check type="checkbox" label="mobile" name="mobile" value="mobile" checked={mobile} onChange={handleChange} inline/>
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="work">
+                 <Form.Check type="checkbox" label="Work" name="work" value="work" checked={work} onChange={handleChange} inline/>
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="motor">
+                <Form.Check type="checkbox" label="motor" name="motor" value="motor" checked={motor} onChange={handleChange} inline/>
+            </Form.Group>
+            
+            </Stack>
+       
         </Form>
 }
 
