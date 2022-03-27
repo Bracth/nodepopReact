bb1e0dd0893b36752cd071b2b6ec7052feeeba33
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-03-27 10:54:27 +0200

    Set a comfirmation notification for the logout button

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,15 +4,28 @@ import Stack from "react-bootstrap/Stack"
 import Button from "react-bootstrap/esm/Button";
 import { logout } from "../auth/service";
 import { useAuth } from "../auth/context";
+import { useState } from "react";
 
+import Notification from "../adverts/notification/Notification"
 import "./header.css"
 
 function Header() {
     const { isLogged, handleLogout } = useAuth();
+    
+    const [needConfirm, setNeedconfirm] = useState(null)
+    
+    const handleConfirmation = async (sure) => {
+        if (sure) {
+            await logout();
+            setNeedconfirm(false);
+            handleLogout();
+        } else {
+            setNeedconfirm(false);
+        }
+    }
 
-    const handleLogoutClick = async() => {
-        await logout();
-        handleLogout();
+    const handleLogoutClick = () => {
+        setNeedconfirm(true);
     }
     
     return <header className="mb-3 mt-3 border-botton-5">
@@ -25,6 +38,7 @@ function Header() {
                         <Button variant="outline-secondary" as={Link} to="/register">Register</Button></>}
             </Stack>
         </Container>
+        {needConfirm? <Notification handleConfirmation={handleConfirmation}>Are you sure you want to logout?</Notification> : null}
     </header>
 }
 
