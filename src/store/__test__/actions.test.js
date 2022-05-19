7769cd9121a5a073a2347a205d73f523d1a38b98
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-05-19 14:05:40 +0200

    Testing sync actions

---
+++ src/store/__test__/actions.test.js
@@ -0,0 +1,24 @@
+import { authLogin, authLoginRequest, advertsLoadedSuccess } from "../actions";
+import { ADVERTS_LOADED_SUCCESS, AUTH_LOGIN_REQUEST } from "../types";
+
+describe("authLoginRequest", () => {
+  test("should return an AUTH_LOGIN_REQUEST action", () => {
+    const expectedAction = {
+      type: AUTH_LOGIN_REQUEST,
+    };
+    const result = authLoginRequest();
+    expect(result).toEqual(expectedAction);
+  });
+});
+
+describe("advertsLoadedSuccess", () => {
+  test("should return a ADVERT_LOADED_SUCCESS action", () => {
+    const adverts = "adverts";
+    const expectedAction = {
+      type: ADVERTS_LOADED_SUCCESS,
+      payload: adverts,
+    };
+    const result = advertsLoadedSuccess(adverts);
+    expect(result).toEqual(expectedAction);
+  });
+});
